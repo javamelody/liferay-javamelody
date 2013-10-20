4aed62a209cf6d4e7b5b20a00b7dab3a4bcd2a10
evernat <evernat@free.fr>
2013-10-20 17:20:01 +0200

    JavaMelody plugin for Liferay

---
+++ README.md
@@ -0,0 +1,31 @@
+liferay-javamelody
+=========================
+
+Plugin for JavaMelody to monitor performance in a [Liferay](http://www.liferay.com/) server 6.1 or later.
+
+See http://javamelody.googlecode.com
+
+Continuous integration: https://javamelody.ci.cloudbees.com/job/liferay-javamelody/
+
+License LGPL, http://www.gnu.org/licenses/lgpl-3.0.txt
+
+Please submit github pull requests and github issues.
+
+
+Downloading and Installing the plugin:
+---------------------------------------
+ - download the latest liferay-javamelody-hook war file from [releases](https://github.com/evernat/liferay-javamelody/releases)
+ - copy the file into the "deploy" directory of your Liferay server and wait a few seconds
+ - Liferay automatically deploys the file and removes it from "deploy" 
+ - open http://localhost:8080/monitoring in a browser
+
+
+Compiling and Installing the plugin:
+---------------------------------------
+ - Install maven
+ - Clone the repository
+ - Compile and test the code, then generate the jar:
+	-> run "mvn clean install" command in your terminal
+ - copy the war file (in the new generated target folder) into the "deploy" directory of your Liferay server and wait a few seconds
+ - Liferay automatically deploys the file and removes it from "deploy" 
+ - open http://localhost:8080/monitoring in a browser
---
+++ src/main/java/net/bull/javamelody/LiferayMonitoringFilter.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2008-2013 by Emeric Vernat
+ *
+ *     This file is part of Java Melody.
+ *
+ * Java Melody is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Java Melody is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Java Melody.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package net.bull.javamelody;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.model.RoleConstants;
+import com.liferay.portal.service.UserLocalServiceUtil;
+import com.liferay.portal.util.PortalUtil;
+
+/**
+ * Filter of monitoring JavaMelody for Liferay.
+ * 
+ * @author Emeric Vernat
+ */
+public class LiferayMonitoringFilter extends PluginMonitoringFilter {
+	/** {@inheritDoc} */
+	@Override
+	public void init(FilterConfig config) throws ServletException {
+		super.init(config);
+
+		LOG.debug("JavaMelody is monitoring Liferay");
+	}
+
+	/** {@inheritDoc} */
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
+		if (!(request instanceof HttpServletRequest)) {
+			super.doFilter(request, response, chain);
+			return;
+		}
+		final HttpServletRequest httpRequest = (HttpServletRequest) request;
+		final HttpServletResponse httpResponse = (HttpServletResponse) response;
+		if (httpRequest.getRequestURI().equals(getMonitoringUrl(httpRequest))) {
+			try {
+				if (!isAdmin(httpRequest)) {
+					LOG.debug("Forbidden access to monitoring from " + request.getRemoteAddr());
+					httpResponse.sendError(HttpServletResponse.SC_FORBIDDEN, "Forbidden access");
+					httpResponse.flushBuffer();
+					return;
+				}
+			} catch (final Exception e) {
+				throw new ServletException(e);
+			}
+		}
+
+		super.doFilter(request, response, chain);
+	}
+	
+	private boolean isAdmin(HttpServletRequest httpRequest) throws PortalException, SystemException {
+		final long userId = PortalUtil.getUserId(httpRequest);
+		final long companyId = PortalUtil.getDefaultCompanyId();
+		return UserLocalServiceUtil.hasRoleUser(companyId, RoleConstants.ADMINISTRATOR, userId, true);
+	}
+}
